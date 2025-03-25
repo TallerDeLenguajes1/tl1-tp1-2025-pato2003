5b6bbfdbdc128af9acc99174b948de2df729c8b2
pato2003 <patogabrielferreyra@gmail.com>
2025-03-25 16:00:30 -0300

    ejercicio de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -3,7 +3,13 @@
 // using namespace std
 
 
-void main()
-{
-    printf("hola mundo");
+void main(){
+    int num1 = 1;
+    int *puntero;
+    puntero = &num1;
+
+
+    printf("Contenido del Puntero: %d\nDireccion del contenido del puntero: %p\n", *puntero, puntero);
+    printf("Direccion de memoria de la variable: %p\nDireccion de memoria del puntero: %p\n", &num1, &puntero);
+    printf("Tamaño de memoria de la variable: %d\n", sizeof(num1));
 }
